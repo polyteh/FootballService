8713c3708f0e9cbd1d66534cc6beeefd32a262d0
Ivan_Prevo <ivan.prevo@ideo-digital.com>
2021-05-13 14:46:10 +0300

    configure startup

--- Football.Service/Startup.cs
+++ Football.Service/Startup.cs
@@ -3,10 +3,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Football.Service
 {
@@ -16,6 +12,9 @@ namespace Football.Service
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // controllers
+            services
+                .AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,10 +25,13 @@ namespace Football.Service
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseCors();
+            app.UseHttpsRedirection();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
