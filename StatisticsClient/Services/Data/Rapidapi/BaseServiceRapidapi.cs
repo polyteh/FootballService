63bb5c74edf6a6e570f88a87631f46fd7e59e119
Ivan_Prevo <ivan.prevo@ideo-digital.com>
2021-05-18 15:21:34 +0300

    get leaque data from rapidapi

---
+++ StatisticsClient/Services/Data/Rapidapi/BaseServiceRapidapi.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Options;
+using RestSharp;
+using StatisticsClient.Configuration;
+using StatisticsClient.Interfaces.Shared;
+
+namespace StatisticsClient.Services.Data.Rapidapi
+{
+    public abstract class BaseServiceRapidapi
+    {
+        protected readonly RestClient _client;
+        protected readonly RapidapiConfiguration _rapidapiConfiguration;
+        protected readonly ISendRequest _sendRequest;
+        public BaseServiceRapidapi(IOptionsSnapshot<RapidapiConfiguration> rapidapiConfiguration,
+            ISendRequest sendRequest)
+        {
+            _rapidapiConfiguration = rapidapiConfiguration.Value;
+            _sendRequest = sendRequest;
+
+            if (_rapidapiConfiguration != null)
+            {
+                _client = new RestClient(_rapidapiConfiguration.BaseUrl);
+            }
+        }
+    }
+}
--- StatisticsClient/Services/Data/Rapidapi/StatisticsLeagueServiceRapidapi.cs
+++ StatisticsClient/Services/Data/Rapidapi/StatisticsLeagueServiceRapidapi.cs
@@ -9,22 +9,19 @@ using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using StatisticsClient.Extensions;
+using StatisticsClient.Services.Data.Rapidapi;
+using StatisticsClient.Interfaces.Shared;
 
 namespace StatisticsClient.Services.DataServices.Rapidapi
 {
-    public class StatisticsLeagueServiceRapidapi : IStatisticsLeagueService
+    public class StatisticsLeagueServiceRapidapi : BaseServiceRapidapi, IStatisticsLeagueService
     {
-        private readonly RestClient _client;
-        private readonly RapidapiConfiguration _rapidapiConfiguration;
-        public StatisticsLeagueServiceRapidapi(IOptionsSnapshot<RapidapiConfiguration> rapidapiConfiguration)
+        public StatisticsLeagueServiceRapidapi(IOptionsSnapshot<RapidapiConfiguration> rapidapiConfiguration, 
+            ISendRequest sendRequest) : base(rapidapiConfiguration, sendRequest)
         {
-            _rapidapiConfiguration = rapidapiConfiguration.Value;
 
-            if (_rapidapiConfiguration != null)
-            {
-                _client = new RestClient(_rapidapiConfiguration.BaseUrl);
-            }
         }
+
         public async Task<LeagueClientModel> GetLeagueDetails(int id)
         {
             var leaguesDetail = new LeagueRapidapiModel();
@@ -37,9 +34,10 @@ namespace StatisticsClient.Services.DataServices.Rapidapi
 
             var request = new RestRequest("standings", Method.GET);
             request.AddRapidapiHeaders(_rapidapiConfiguration);
-            request.AddUrlSegment("season", 2020);
-            request.AddUrlSegment("league", 39);
+            request.AddParameter("season", 2020);
+            request.AddParameter("league", 39);
 
+            var resultt = await _sendRequest.SendLoggedRequest<List<LeagueRapidapiModel>>(_client, request);
 
             var result = new LeagueClientModel()
             {
